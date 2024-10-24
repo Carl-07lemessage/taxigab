4c7631062439304fe42455b7674b44253390a33f
landou <landoumechack@gmail.com>
2024-10-24 19:45:31 +0100

    components location ride configs

---
+++ components/configs/country-list.tsx
@@ -0,0 +1,3 @@
+export const countryItems = [
+    { label: "+241", value: "241" },
+  ];
--- components/login/signin.text.tsx
+++ components/login/signin.text.tsx
@@ -1,39 +1,38 @@
 import { View, Text } from "react-native";
-import React from "react"; // Importation de React pour l'utilisation des composants fonctionnels
-import { commonStyles } from "@/styles/common.style"; // Importation des styles communs partagés
-import { external } from "@/styles/external.style"; // Importation des styles externes
+import React from "react"; 
+import { commonStyles } from "@/styles/common.style";
+import { external } from "@/styles/external.style"; 
 
-// Définition du composant fonctionnel SignInText
+
+
 export default function SignInText({
-  title, // Titre optionnel passé en tant que propriété
-  subtitle, // Sous-titre optionnel passé en tant que propriété
+  title, 
+  subtitle, 
 }: {
-  title?: string; // Le titre peut être une chaîne de caractères ou indéfini
-  subtitle?: string; // Le sous-titre peut être une chaîne de caractères ou indéfini
+  title?: string; 
+  subtitle?: string; 
 }) {
   return (
     <>
-      {/* Affichage du titre */}
       <Text
         style={[
-          commonStyles.regularTextBigBlack, // Style pour le texte principal
-          { color: "#000" }, // Couleur du texte noir
-          { textAlign: "left" }, // Alignement du texte à gauche
-          { marginTop: 20 }, // Marge supérieure de 20 unités
+          commonStyles.regularTextBigBlack, 
+          { color: "#000" },
+          { textAlign: "left" }, 
+          { marginTop: 20 }, 
         ]}
       >
-        {/* Si un titre est fourni, l'afficher, sinon afficher le texte par défaut */}
+
+
         {title ? title : "Join to take your ride 😃"}
       </Text>
-      {/* Affichage du sous-titre */}
       <Text
         style={[
-          commonStyles.regularText, // Style pour le texte secondaire
-          external.pt_4, // Style de padding supérieur défini dans les styles externes
-          { textAlign: "left" }, // Alignement du texte à gauche
-        ]}
+          commonStyles.regularText,
+          external.pt_4,
+          { textAlign: "left" },
+          
       >
-        {/* Si un sous-titre est fourni, l'afficher, sinon afficher le texte par défaut */}
         {subtitle
           ? subtitle
           : "Enter your phone number, get one otp and let's go 😍"}
